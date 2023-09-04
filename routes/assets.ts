def1c09771ffa58be95b081cf927cf68e49c1c52
peron <peron_ap@icloud.com>
2023-09-04 23:20:49 +0800

    feat: include tailwind plugins

--- routes/assets.ts
+++ routes/assets.ts
@@ -2,7 +2,9 @@ import { Hono } from "hono/mod.ts";
 import { lazy } from "$/utils/lazy.ts";
 
 const tailwindcssScript = lazy(() =>
-  fetch("https://cdn.tailwindcss.com/3.3.3")
+  fetch(
+    "https://cdn.tailwindcss.com/3.3.3?plugins=forms,typography,aspect-ratio",
+  )
 );
 const htmxScript = lazy(() =>
   fetch("https://unpkg.com/htmx.org@1.9.5/dist/htmx.min.js")
