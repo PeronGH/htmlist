5fb5c2e083f0ca977315885f64667e693c764d84
peron <peron_ap@icloud.com>
2023-09-08 15:28:57 +0800

    fix: try fixing deno deploy error

--- utils/fs_routing.ts
+++ utils/fs_routing.ts
@@ -1,16 +1,17 @@
 import { Hono } from "hono/mod.ts";
-import { parse, relative, resolve } from "std/path/mod.ts";
+import { dirname, parse, relative, resolve } from "std/path/mod.ts";
 import { walk } from "std/fs/walk.ts";
 
 export async function initFsRouting(hono: Hono, importUrl: string) {
-  const { pathname } = new URL(importUrl);
-  const routesFolder = resolve(pathname, "../routes");
+  const mainPath = new URL(importUrl).pathname;
+  const routesFolder = resolve(dirname(mainPath), "./routes");
   for await (
-    const file of walk(routesFolder, {
-      includeDirs: false,
-    })
+    const file of walk(routesFolder, { includeDirs: false })
   ) {
-    const router = (await import(file.path)).default;
+    const currentPath = new URL(import.meta.url).pathname;
+    const importPath = relative(dirname(currentPath), file.path);
+
+    const router = (await import(importPath)).default;
     if (!(router instanceof Hono)) continue;
 
     const relativePath = relative(routesFolder, file.path);
