334957c4aae6644aca6c31f4bcf023c5ac70e06e
peron <peron_ap@icloud.com>
2023-09-08 15:47:49 +0800

    Revert "revert: remove `hx` to keep it minimal"
    This reverts commit 5741b464673a73d4f863bf706f7c0aa48f8de200.

--- server.tsx
+++ server.tsx
@@ -1,27 +1,30 @@
 import "std/dotenv/load.ts";
 import { Hono } from "hono/mod.ts";
 import { Page } from "$/htmx/page.tsx";
-import { serveStatic } from "hono/middleware.ts";
-import { zValidator } from "@hono/zod-validator";
-import { z } from "zod";
-import { initFsRouting } from "$/utils/fs_routing.ts";
+import { assets } from "$/routes/assets.ts";
 
 const app = new Hono();
 
-await initFsRouting(app, import.meta.url);
+app.route("/assets", assets);
 
 // Your code goes here:
+
+const returnDate: HxHandler = (ctx) =>
+  ctx.html(
+    <p>
+      Loaded at {new Date().toString()}
+    </p>,
+  );
 
 app.get("/", (ctx) =>
   ctx.html(
     <Page title="Index Page">
       <div class="container mx-auto p-4">
         <button
-          id="load-btn"
           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           hx-get="/load"
-          hx-target="#content"
-          hx-swap="outerHTML"
+          hx-target="#replaceMe"
+          hx-swap="innerHTML"
         >
           Load
         </button>
@@ -32,29 +35,12 @@ app.get("/", (ctx) =>
 
 app.get(
   "/load",
-  zValidator("query", z.object({ defaultName: z.string().default("you") })),
   (ctx) =>
     ctx.html(
-      <div
-        id="content"
-        class="mt-4 p-2 border-4 border-indigo-500 rounded"
-        _="init put 'Reload' into #load-btn"
-      >
-        <input
-          type="text"
-          name="name"
-          value={ctx.req.valid("query").defaultName}
-        />
-        <button
-          class="ml-2 p-2 border-2 border-indigo-500 rounded"
-          _="on click put `Hi, ${<input/>'s value}!` into #content"
-        >
-          Greet
-        </button>
+      <div id="replaceMe" class="mt-4 p-2 border-4 border-indigo-500 rounded">
+        Loaded at {new Date().toString()}
       </div>,
     ),
 );
-
-app.get("*", serveStatic({ root: "./static/" }));
 
 Deno.serve(app.fetch);
